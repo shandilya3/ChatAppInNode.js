c8eaf737677c3da3eb819a572625721ff3eec695
Shandilya <happyshandilya@gmail.com>
2018-02-20 19:13:43 -0700

    some changes for correct port

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-app.get('/', function(req, res){
-  res.sendFile('/Users/happy.shandilya/Documents/serverside/ChatServer/index.html');
+var express = require('express'),
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io').listen(server);
+
+server.listen(8000);
+
+app.use(express.static('./'));
+
+app.get('/',function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection',function(socket){
+  console.log('SOMEONE CONNECTED:', socket.id);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    console.log( io.emit(msg));
-    io.emit(msg);
+  socket.on('send', function(data){
+    console.log("what is this" + data);
+    io.sockets.emit('new', data);
+   //socket.broadcast.emit("new broadcat", data)
   });
 });
-   
-
-http.listen(8000, function(){
-  console.log("listening on *: 8000");
-})
